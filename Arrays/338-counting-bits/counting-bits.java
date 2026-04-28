3743fae5f53c7aaee477885f50d5952279e343aa
radhika0699 <48384550+radhika0699@users.noreply.github.com>
2026-04-28 20:27:25 +0530

    Time: 2 ms (96.55%) | Memory: 48.6 MB (70.01%) - LeetSync

--- Arrays/338-counting-bits/counting-bits.java
+++ Arrays/338-counting-bits/counting-bits.java
@@ -1,20 +1,17 @@
 class Solution {
+    /*
+    i = 6 (110)
+    i >> 1 = 3 (11)
+
+    ans[6] = ans[3] + 0 = 2
+    */
     public int[] countBits(int n) {
         int[] a = new int[n+1];
         for(int i = 0; i<=n; i++)
         {
-            a[i] = hammingWeight(i);
+            a[i] = a[i >> 1] + (i&1);
         }   
         return a;
     }
-    private int hammingWeight(int n)
-    {
-        int ct = 0;
-        while(n!=0)
-        {
-            n = n & (n-1);
-            ct++;
-        }
-        return ct;
-    }
+
 }
