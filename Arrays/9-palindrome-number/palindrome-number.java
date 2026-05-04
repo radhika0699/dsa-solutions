775a3dbb0a597ce29b1c8bdc5ac158ddbe4cf31a
radhika0699 <48384550+radhika0699@users.noreply.github.com>
2026-05-04 19:33:28 +0530

    Time: 7 ms (13.74%) | Memory: 46 MB (53.61%) - LeetSync

---
+++ Arrays/9-palindrome-number/palindrome-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    //2 pointers
+    public boolean isPalindrome(int x) {
+        String s = String.valueOf(x);
+        int l = 0, r = s.length()-1;
+        while(l <= r)
+        {
+            if(s.charAt(l) != s.charAt(r))
+                return false;
+            l++;
+            r--;
+        }
+        return true;
+    }
+}
